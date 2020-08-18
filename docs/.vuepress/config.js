545b758508bf0d823a6b7832e699517d504f3682
shanejixx <shanejix@vip.163.com>
2020-08-18 21:38:52 +0800

    ;docs:docs(all):update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,41 +22,52 @@ module.exports = {
       "/main/": [
         {
           title: "Favorite APP",
-          collapsable: false,
-          path: "favoriteApp/current",
+          collapsable: true,
+          path: "FavoriteApp/current",
           // children: [["/favoriteApp/current", "front end"]],
         },
         {
           title: "VSCode 相关",
-          collapsable: false,
+          collapsable: true,
           children: [
             {
               title: "vscode_plugins",
-              path: "vscode/vscode_plugins",
+              path: "VSCode/vscode_plugins",
             },
             {
               title: "enable_preview",
-              path: "vscode/enable_preview",
+              path: "VSCode/enable_preview",
             },
           ],
         },
         {
           title: "Ubuntu 相关",
-          collapsable: false,
+          collapsable: true,
           children: [
-            ["ubuntu/ctrl_alt_arrow_conflict", "ctrl_alt_arrow_conflict"],
-            ["ubuntu/space_small", "space_small"],
-            ["ubuntu/theme", "theme"],
+            ["Ubuntu/ctrl_alt_arrow_conflict", "ctrl_alt_arrow_conflict"],
+            ["Ubuntu/space_small", "space_small"],
+            ["Ubuntu/theme", "theme"],
+          ],
+        },
+        {
+          title: "Front End",
+          collapsable: true,
+          children: [
+            [
+              "FrontEnd/useful_ebsites",
+              "useful_websites_every_front-end_developer_hould_know_about",
+            ],
           ],
         },
         {
           title: "Other 其他",
-          collapsable: false,
+          collapsable: true,
           children: [
             [
-              "other/unicode编码后的汉字JS转换方法.md",
+              "Other/unicode编码后的汉字JS转换方法.md",
               "&#x(unicode编码后的汉字)JS转换方法",
             ],
+            ["Other/stash.md", "stash"],
           ],
         },
       ],
---
+++ docs/main/FrontEnd/useful_ebsites.md
@@ -0,0 +1,9 @@
+
+DEV Community
+- [https://dev.to/](https://dev.to/)
+
+CSS-tricks
+- [https://css-tricks.com/](https://css-tricks.com/)
+
+30secondsofcode
+- [https://www.30secondsofcode.org/](https://www.30secondsofcode.org/)
